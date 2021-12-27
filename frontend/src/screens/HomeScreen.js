8b51d9d209bb3ee3987993c39ebe5b71b4d60a1a
Bilal Afzal <bilalbal914@gmail.com>
2021-12-27 22:31:17 +0500

    axios request

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import data from '../data';
+import React, { useEffect, useState } from 'react';
+
 import Product from '../components/Product';
-export default function HomeScreen() {
+import axios from 'axios'//for requesting data from backend to frontend
+export default function HomeScreen() {//fetcing data using backend to frontend
+  const [products,setProducts]=useState([]);
+  useEffect(() => {
+    const fetchdata= async () => {
+          const {data} =await axios.get('api/products');
+          setProducts(data);
+    };
+    fetchdata();
+    
+  }, [])
     return (
         <div>
         <div className="row center">
           {
-            data.products.map((product) => (
+            products.map((product) => (
               <Product key={product._id} product={product}></Product>//extracting component from product
             ))}
          
