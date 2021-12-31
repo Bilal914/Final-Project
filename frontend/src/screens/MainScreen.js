61548905320c398389fe2a701fdc3d9e7b2cbff7
Bilal Afzal <bilalbal914@gmail.com>
2021-12-31 21:46:50 +0500

    commit

--- frontend/src/screens/MainScreen.js
+++ frontend/src/screens/MainScreen.js
@@ -1,11 +1,25 @@
 import React from 'react'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { Carousel } from 'react-bootstrap';
+import Image1 from './picture14.jpg';
 export default function MainScreen() {
     return (
         <div>
-           
+            <Carousel>
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={Image1}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>Men,Women and Children</h3>
+      
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+</Carousel>
         </div>
     )
 }
